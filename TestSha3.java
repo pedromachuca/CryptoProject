28fc38e9a852651caeed0589cc43195480ad179a
root <root@localhost.localdomain>
2018-12-01 10:24:56 -0500

    First block added

--- TestSha3.java
+++ TestSha3.java
@@ -3,8 +3,31 @@ import org.bouncycastle.jcajce.provider.digest.SHA3.DigestSHA3;
 import org.bouncycastle.jcajce.provider.digest.SHA3.Digest256;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPairGenerator;
+import java.security.Security;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPoint;
+import java.security.spec.ECPublicKeySpec;
+
+import javax.crypto.Cipher;
+
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.jce.spec.ECNamedCurveSpec;
+import org.bouncycastle.util.encoders.Hex;
 
  import java.security.*;
+ import java.security.spec.ECGenParameterSpec;
+
+
+// import org.bouncycastle.jce.spec.ECGenParameterSpec;
+
+
 // import java.math.BigInteger;
 // import org.bouncycastle.math.ec.*;
 // import org.bouncycastle.jce.spec.*;
@@ -35,24 +58,52 @@ public class TestSha3 {
   	byte[] b1 = new byte[32];
   	byte[] b2 = new byte[32];
   	int id=1;
-  	b1 =zeroByte(h1tmp, h0, id);
+    String H = "a";
+  	b1 =zeroByte(h1tmp, h0, id, H);
   	String Bh1 = hashToString(b1);
   	id=2;
-  	b2 =zeroByte(Bh1, h0, id);
+    H= "b";
+  	b2 =zeroByte(Bh1, h0, id, H);
 
     System.out.println("\n*** Key Generation ***");
-    System.out.println("Wallet 1 = vk1 = ");
-    System.out.println("Wallet 2 = vk2 = ");
-    System.out.println("Wallet 3 = vk3 = ");
 
     try{
 
-      KeyPair pair =generateKeyPair();
-      PrivateKey sk1 = pair.getPrivate();
-      PublicKey vk1 = pair.getPublic();
-      System.out.println("sk1 :"+sk1);
-      System.out.println("vk1 :"+vk1);
+      KeyPair pair1 =generateKeyPair();
+      KeyPair pair2 =generateKeyPair();
+      KeyPair pair3 =generateKeyPair();
 
+      PrivateKey sk1 = pair1.getPrivate();
+      PublicKey vk1 = pair1.getPublic();
+      byte [] pub1 =vk1.getEncoded();
+      String vk1hextmp = Hex.toHexString(pub1);
+      String[] vk1hex = vk1hextmp.split("3049301306072a8648ce3d020106082a8648ce3d0301010332000");
+
+      PrivateKey sk2 = pair2.getPrivate();
+      PublicKey vk2 = pair2.getPublic();
+      byte [] pub2 =vk2.getEncoded();
+      String vk2hextmp = Hex.toHexString(pub2);
+      String[] vk2hex = vk2hextmp.split("3049301306072a8648ce3d020106082a8648ce3d0301010332000");
+
+      PrivateKey sk3 = pair3.getPrivate();
+      PublicKey vk3 = pair3.getPublic();
+      byte [] pub3 =vk3.getEncoded();
+      String vk3hextmp = Hex.toHexString(pub3);
+      String[] vk3hex = vk3hextmp.split("3049301306072a8648ce3d020106082a8648ce3d0301010332000");
+
+      System.out.println("Wallet 1 = vk1 = "+vk1hex[1]);
+      System.out.println("Wallet 2 = vk2 = "+vk2hex[1]);
+      System.out.println("Wallet 3 = vk3 = "+vk3hex[1]);
+
+      System.out.println("\n*** BlockChain of Transactions  ***");
+      System.out.println("Init the Hash Chain: h0 = "+h0);
+      String block = vk1hex[1]+" receives 10 Euros";
+      //System.out.println("Add a New Block '"+block+"'");
+      id =1;
+      H =block;
+      zeroByte(block, h0, id, H);
+      // String
+      // generateSignature(sk1, )
     }catch(GeneralSecurityException e){
       System.out.println(e.getMessage());
     }
@@ -60,10 +111,12 @@ public class TestSha3 {
 
   }
 
+
   public static KeyPair generateKeyPair()throws GeneralSecurityException{
 
+    ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
     KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
-    keyGen.initialize(384, new SecureRandom());
+    keyGen.initialize(ecSpec, new SecureRandom());
     KeyPair pair = keyGen.generateKeyPair();
     return pair;
   }
@@ -71,7 +124,7 @@ public class TestSha3 {
 
   public static byte[] generateSignature(PrivateKey ecPrivate, byte[] input) throws GeneralSecurityException{
 
-    Signature signature = Signature.getInstance("SHA384withECDSA", "BCFIPS");
+    Signature signature = Signature.getInstance("SHA384withECDSA", "BC");
     signature.initSign(ecPrivate);
     signature.update(input);
     return signature.sign();
@@ -86,7 +139,7 @@ public class TestSha3 {
     return signature.verify(encSignature);
   }
 
-	public static byte [] zeroByte(String hr, String h0, int id){
+	public static byte [] zeroByte(String hr, String h0, int id, String H){
 		boolean zero = true;
 		byte [] b = new byte[8];
 		byte [] b1 = new byte[32];
@@ -104,8 +157,14 @@ public class TestSha3 {
 		}while(zero);
 
 		String Bh1 = hashToString(b1);
-    System.out.println("Add a New Block 'a' : h"+id+" = SHA3("+h0+"\n\t\t\t\t|| a || "+r+")");
-    System.out.println("\t\t\t = "+Bh1);
+    if(H.equals("a")||H.equals("b")){
+      System.out.println("Add a New Block '"+H+"' : h"+id+" = SHA3("+h0+"\n\t\t\t\t|| "+H+" || "+r+")");
+      System.out.println("\t\t\t = "+Bh1);
+    }
+    else {
+      System.out.println("Add a New Block '"+H+"' \n   -> h"+id+" = SHA3("+h0+"\n\t\t|| "+H+"\n\t\t|| "+r+")");
+      System.out.println("\t      = "+Bh1);
+    }
 		return b1;
 	}
 
