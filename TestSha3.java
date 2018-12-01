630989edd2b1722b10576603fbb2fd5c754b0cbb
root <root@localhost.localdomain>
2018-12-01 06:30:30 -0500

    key generation

--- TestSha3.java
+++ TestSha3.java
@@ -1,43 +1,98 @@
 import java.security.MessageDigest;
-
 import org.bouncycastle.jcajce.provider.digest.SHA3.DigestSHA3;
 import org.bouncycastle.jcajce.provider.digest.SHA3.Digest256;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+
+ import java.security.*;
+// import java.math.BigInteger;
+// import org.bouncycastle.math.ec.*;
+// import org.bouncycastle.jce.spec.*;
 
 import java.util.*;
-//
+
 public class TestSha3 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		System.out.println("\n*** SHA3 Vector Tests ***");
-        System.out.println("''    -> "+sha3String(""));
-        System.out.println("'abc' -> "+sha3String("abc")+"\n");
+    Security.addProvider(new BouncyCastleProvider());
 
-		System.out.println("*** Hash Chain ***");
-		String h0 = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
-        System.out.println("Init the Hash Chain : h0 = "+h0);
-		String h1tmp = h0+"a";
-		String h1 = sha3String(h1tmp);
-        System.out.println("Add a New Block 'a' : h1 = "+h1);
-		String h2 = h1+"b";
-        System.out.println("Add a New Block 'b' : h2 = "+sha3String(h2)+"\n");
-		
-		System.out.println("*** Hash Chain with Proof of Work ***");
-        System.out.println("Init the Hash Chain : h0 = "+h0);
-		byte[] b1 = new byte[32];
-		byte[] b2 = new byte[32];
-		int id=1;
-		b1 =zeroByte(h1tmp, h0, id);
-		String Bh1 = hashToString(b1);
-		id=2;
-		b2 =zeroByte(Bh1, h0, id);
+    System.out.println("\n*** SHA3 Vector Tests ***");
+    System.out.println("''    -> "+sha3String(""));
+    System.out.println("'abc' -> "+sha3String("abc")+"\n");
+
+    System.out.println("*** Hash Chain ***");
+  	String h0 = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+    System.out.println("Init the Hash Chain : h0 = "+h0);
+  	String h1tmp = h0+"a";
+  	String h1 = sha3String(h1tmp);
+    System.out.println("Add a New Block 'a' : h1 = "+h1);
+  	String h2 = h1+"b";
+    System.out.println("Add a New Block 'b' : h2 = "+sha3String(h2)+"\n");
+
+  	System.out.println("*** Hash Chain with Proof of Work ***");
+    System.out.println("Init the Hash Chain : h0 = "+h0);
+  	byte[] b1 = new byte[32];
+  	byte[] b2 = new byte[32];
+  	int id=1;
+  	b1 =zeroByte(h1tmp, h0, id);
+  	String Bh1 = hashToString(b1);
+  	id=2;
+  	b2 =zeroByte(Bh1, h0, id);
+
+    System.out.println("\n*** Key Generation ***");
+    System.out.println("Wallet 1 = vk1 = ");
+    System.out.println("Wallet 2 = vk2 = ");
+    System.out.println("Wallet 3 = vk3 = ");
+
+    try{
+
+      KeyPair pair =generateKeyPair();
+      PrivateKey sk1 = pair.getPrivate();
+      PublicKey vk1 = pair.getPublic();
+      System.out.println("sk1 :"+sk1);
+      System.out.println("vk1 :"+vk1);
+
+    }catch(GeneralSecurityException e){
+      System.out.println(e.getMessage());
     }
+
+
+  }
+
+  public static KeyPair generateKeyPair()throws GeneralSecurityException{
+
+    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+    keyGen.initialize(384, new SecureRandom());
+    KeyPair pair = keyGen.generateKeyPair();
+    return pair;
+  }
+
+
+  public static byte[] generateSignature(PrivateKey ecPrivate, byte[] input) throws GeneralSecurityException{
+
+    Signature signature = Signature.getInstance("SHA384withECDSA", "BCFIPS");
+    signature.initSign(ecPrivate);
+    signature.update(input);
+    return signature.sign();
+  }
+
+
+  public static boolean verifySignature(PublicKey ecPublic, byte[] input, byte[] encSignature) throws GeneralSecurityException{
+
+    Signature signature = Signature.getInstance("SHA384withECDSA", "BCFIPS");
+    signature.initVerify(ecPublic);
+    signature.update(input);
+    return signature.verify(encSignature);
+  }
+
 	public static byte [] zeroByte(String hr, String h0, int id){
 		boolean zero = true;
 		byte [] b = new byte[8];
 		byte [] b1 = new byte[32];
 		String hr1;
 		String r;
+
 		do{
 			b = generateRand();
 			r = hashToString(b);
@@ -47,11 +102,14 @@ public class TestSha3 {
 				zero =false;
 			}
 		}while(zero);
+
 		String Bh1 = hashToString(b1);
-        System.out.println("Add a New Block 'a' : h"+id+" = SHA3("+h0+"\n\t\t\t\t|| a || "+r+")");
-        System.out.println("\t\t\t = "+Bh1);
+    System.out.println("Add a New Block 'a' : h"+id+" = SHA3("+h0+"\n\t\t\t\t|| a || "+r+")");
+    System.out.println("\t\t\t = "+Bh1);
 		return b1;
 	}
+
+
 	public static byte [] generateRand(){
 
 		byte [] b = new byte[8];
@@ -59,32 +117,32 @@ public class TestSha3 {
 		return b;
 	}
 
-    static String sha3String(String input) {
+  static String sha3String(String input) {
 
-        DigestSHA3 sha3 = new Digest256();
-        sha3.update(input.getBytes());
-        return TestSha3.hashToString(sha3);
-    }
+    DigestSHA3 sha3 = new Digest256();
+    sha3.update(input.getBytes());
+    return TestSha3.hashToString(sha3);
+  }
 
-    static byte [] sha3byte(String input) {
+  static byte [] sha3byte(String input) {
 
-        DigestSHA3 sha3 = new Digest256();
-        sha3.update(input.getBytes());
-		byte [] b =sha3.digest();	
+    DigestSHA3 sha3 = new Digest256();
+    sha3.update(input.getBytes());
+		byte [] b =sha3.digest();
 		return b;
 	}
 
-    static String hashToString(MessageDigest hash) {
-        return hashToString(hash.digest());
-    }
-	
-    static String hashToString(byte[] hash) {
-        StringBuffer buff = new StringBuffer();
+  static String hashToString(MessageDigest hash) {
+    return hashToString(hash.digest());
+  }
 
-        for (byte b : hash) {
-            buff.append(String.format("%02x", b & 0xFF));
-        }
+  static String hashToString(byte[] hash) {
 
-        return buff.toString();
+    StringBuffer buff = new StringBuffer();
+
+    for (byte b : hash) {
+        buff.append(String.format("%02x", b & 0xFF));
     }
+    return buff.toString();
+  }
 }
